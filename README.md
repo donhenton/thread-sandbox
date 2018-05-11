86170e0135911fdc673d567fd509b89243ece2b0
Don Henton <donhenton@gmail.com>
2018-05-11 10:39:57 -0500

    more initialization

---
+++ README.md
@@ -0,0 +1,5 @@
+# Launching demos 
+
+```
+./launch demo1
+```
--- src/main/java/com/dhenton9000/thread/sandbox/BasicApplication.java
+++ src/main/java/com/dhenton9000/thread/sandbox/BasicApplication.java
@@ -2,20 +2,42 @@ package com.dhenton9000.thread.sandbox;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 
 @ComponentScan(basePackages = "com.dhenton9000.thread.sandox")
 @SpringBootApplication
 public class BasicApplication {
-    
-     private final static Logger LOG
-            = LoggerFactory.getLogger(BasicApplication.class);
-    
-    
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
-	}
+    
+    @Value("${app}")
+    private String app;
+    
+    private final static Logger LOG
+            = LoggerFactory.getLogger(BasicApplication.class);
+
+    public static void main(String[] args) {
+        
+         ConfigurableApplicationContext contextVar
+                = SpringApplication.run(BasicApplication.class, args);
+        contextVar.getBean(BasicApplication.class).beginApplication();
+    }
+    
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        LOG.debug("hello world, I have just started up");
+    }
+
+    private void beginApplication() {
+        LOG.debug("app is "+app);
+    }
+    
+    
+    
 }
